71a4fa2d1253fcc6fedf9356603cde24a4818dfa
jo <josehehall98@gmail.com>
2024-07-23 14:42:21 -0600

    inti

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev - v6an8tmyb1oj3ssb.us.auth0.com'
+export const clientId = '0hCDMTdgWypFlRUpMixBIr40AV5acejf'
+export const audience = 'https://superZoomZoom.com'
