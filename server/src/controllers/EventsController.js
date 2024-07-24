797f65ecde4f051382b4a8bf00606fcffed30855
jo <josehehall98@gmail.com>
2024-07-23 18:57:26 -0600

    server side: delete post

--- server/src/controllers/EventsController.js
+++ server/src/controllers/EventsController.js
@@ -6,11 +6,13 @@ export class EventsController extends BaseController {
   constructor() {
     super('api/events')
     this.router
+      .get('/:eventId', this.getEventById)
+      .get('', this.getAllEvents)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createEvent)
+      // .put('/:eventId', this.editEvent);
+      .delete('/:eventId', this.archiveEventById)
   }
-
-
 
   async createEvent(request, response, next) {
     try {
@@ -24,4 +26,58 @@ export class EventsController extends BaseController {
       next(error)
     }
   }
+
+  async getAllEvents(request, response, next) {
+    try {
+      const events = await eventsService.getAllEvents()
+      response.send(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getEventById(request, response, next) {
+    try {
+      const eventId = request.params.eventId
+      const event = await eventsService.getEventById(eventId)
+      response.send(event)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  // async editEvent(request, response, next) {
+  //   try {
+  //     const eventId = request.params.eventId;
+  //     const updatedData = request.body;
+  //     const user = request.userInfo;
+  //     const event = await eventsService.editEvent(eventId, updatedData, user.id);
+  //     response.send(event);
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // }
+
+  async archiveEventById(request, response, next) {
+    try {
+      const user = request.userInfo
+      const eventId = request.params.eventId
+      const message = await eventsService.archiveEventById(eventId, user.id)
+      request.send(message);
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  // async archiveAlbumById(request, response, next) {
+  //   try {
+  //     const user = request.userInfo
+  //     const albumId = request.params.albumId
+  //     const message = await albumsService.archiveAlbumById(albumId, user.id)
+  //     response.send(message)
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
+
 }
--- server/src/services/EventsService.js
+++ server/src/services/EventsService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 class EventsService {
   async createEvent(eventData) {
@@ -6,6 +7,40 @@ class EventsService {
     await event.populate('creator')
     return event
   }
+
+  async getAllEvents() {
+    const events = await dbContext.Events.find().sort('-createdAr').populate('creator')
+    return events
+  }
+
+  async getEventById(eventId) {
+    const event = await dbContext.Events.findById(eventId).populate('creator')
+    if (event == null) throw new Error(`No event with id ${eventId}`)
+    return event
+  }
+  async archiveEventById(eventId, userId) {
+    const eventToArchive = await this.getEventById(eventId)
+    if (userId != eventToArchive.creatorId) throw new Forbidden(`No way man`)
+
+    eventToArchive.isCanceled = !eventToArchive.isCanceled
+    await eventToArchive.save()
+    return `${eventToArchive.name} was archived successfully`
+  }
+
+  // async archiveAlbumById(albumId, userId) {
+  //   const albumToArchive = await this.getAlbumById(albumId)
+  //   if (userId != albumToArchive.creatorId) throw new Forbidden(`You shall not pass!, this is not yours!`)
+
+  //   // await albumToArchive.deleteOne()
+  //   // return `Deleted ${albumToArchive.title}`
+  //   // albumToArchive.archived = true
+  //   albumToArchive.archived = !albumToArchive.archived // flips the bool
+  //   await albumToArchive.save()
+  //   return `${albumToArchive.title} was archived`
+  // }
+
 }
+
+
 
 export const eventsService = new EventsService()
